72c754a41f6cefc7227f4b479d4cc2f1dce9161d
Marcello Bozzi <bozzimarcello@gmail.com>
2024-04-05 10:44:47 +0200

    Package install and first chart

---
+++ MAUICharts/MAUICharts/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MAUICharts
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MAUIChartsDemo
 A demo project to show how to integrate LiveCharts 2 in .NET MAUI
+
+## LiveCharts 2 documentation
+https://livecharts.dev/docs/Maui/2.0.0-rc2/Overview.Installation
